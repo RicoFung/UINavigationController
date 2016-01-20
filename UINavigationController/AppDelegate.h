8a95f33ab13cd78e788860cdb2b528a438764037
RexFun <oleFun@icloud.com>
2016-01-20 16:49:02 +0800

    Initial Commit

---
+++ UINavigationController/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UINavigationController
+//
+//  Created by mac373 on 16/1/20.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UINavigationController/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UINavigationController
+//
+//  Created by mac373 on 16/1/20.
+//  Copyright © 2016年 ole. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
